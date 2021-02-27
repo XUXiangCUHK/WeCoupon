1f6034b264c59882ee248fd1f692675db59484e9
XUXiang <xuxiangcuhk@qq.com>
2021-02-27 21:02:27 +0800

    db for login

--- db/manipulator.py
+++ db/manipulator.py
@@ -7,16 +7,47 @@ class Manipulator:
     def __init__(self):
         self.sql = Sql()
 
-    def insert_account_info(self, user_name, passward, email, pos):
+    def insert_account_info(self, user_name, first_name, last_name, email, SID, password, is_student):
         data = {
             'user_name': user_name,
-            'passward': passward,
+            'first_name': first_name,
+            'last_name': last_name,
             'email': email,
-            'pos': pos
+            'SID': SID,
+            'password': password,
+            'is_student': is_student
         }
         self.sql.insert_account(data)
+    
+    def insert_course_info(self, course_id, course_name, course_instructor, course_token):
+        data = {
+            'course_id': course_id,
+            'course_name': course_name,
+            'course_instructor': course_instructor,
+            'course_token': course_token,
+        }
+        self.sql.insert_course(data)
+    
+    def insert_enrollment_info(self, user_name, course_id):
+        data = {
+            'user_name': user_name,
+            'course_id': course_id,
+        }
+        self.sql.insert_enrollment(data)
+
+    def user_enrollment(self, user_name):
+        res = self.sql.read_user_course(user_name)
+        return res        
         
 
 if __name__ == "__main__":
     m = Manipulator()
-    m.insert_account_info('3100', '3100', '1155107785@link.cuhk.edu.hk', 'T')
+    # m.insert_account_info('StevenXU', 'xu', 'xiang', '1155107785@link.cuhk.edu.hk', '1155107785', 'wecoupon', '1')
+    # m.insert_course_info('CSCI3100', 'Software Engineering', 'Prof. Michael R. Lyu', 'software')
+    # m.insert_course_info('IERG3310', 'Computer Networking', 'Prof. Xing Guoliang', 'networks')
+    # m.insert_course_info('FTEC3001', 'Financial Innovation & Structured Products', 'Prof. Chen Nan', 'FinTech0')
+    # m.insert_enrollment_info('StevenXU', 'CSCI3100')
+    # m.insert_enrollment_info('StevenXU', 'IERG3310')
+    # m.insert_enrollment_info('StevenXU', 'FTEC3001')
+
+    m.user_enrollment('StevenXU')
--- db/sql.py
+++ db/sql.py
@@ -9,12 +9,39 @@ class Sql:
 
     def insert_account(self, data):
         statement = '''
-                    INSERT IGNORE INTO `test`.`account`
-                    (`user_name`, `passward`, `email`, `pos`)
+                    INSERT IGNORE INTO `WeCoupon`.`account`
+                    (`user_name`, `first_name`, `last_name`, `email`, `SID`, `password`, `is_student`)
                     VALUES
-                    (%(user_name)s, %(passward)s, %(email)s, %(pos)s);
+                    (%(user_name)s, %(first_name)s, %(last_name)s, %(email)s, %(SID)s, %(password)s, %(is_student)s);
                     '''
         self.db.single_write_into_mysql(data, statement)
+
+    def insert_course(self, data):
+        statement = '''
+                    INSERT IGNORE INTO `WeCoupon`.`course`
+                    (`course_id`, `course_name`, `course_instructor`, `course_token`)
+                    VALUES
+                    (%(course_id)s, %(course_name)s, %(course_instructor)s, %(course_token)s);
+                    '''
+        self.db.single_write_into_mysql(data, statement)
+    
+    def insert_enrollment(self, data):
+        statement = '''
+                    INSERT IGNORE INTO `WeCoupon`.`enrollment`
+                    (`user_name`, `course_id`)
+                    VALUES
+                    (%(user_name)s, %(course_id)s);
+                    '''
+        self.db.single_write_into_mysql(data, statement)
+
+    def read_user_course(self, user_name):
+        statement = '''
+                    SELECT c.course_id, course_name, course_instructor 
+                    FROM WeCoupon.enrollment AS e
+                    JOIN Wecoupon.course AS c ON e.course_id = c.course_id
+                    WHERE user_name = '{}'
+                    '''.format(user_name)
+        return self.db.read_from_mysql(statement)
 
 if __name__ == "__main__":
     e = Sql()
