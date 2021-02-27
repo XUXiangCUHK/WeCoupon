be2c1e1f41ce30809979d4b60c22bfc4a26f4372
piperzeng <hongni7@126.com>
2021-02-27 16:24:27 +0800

    delete history

--- .history/static/js/login_20210221164458.js
+++
@@ -1,5 +0,0 @@
-function processform(){
-    console.log(document.querySelector("#login"));
-    console.log(document.querySelector("#password"));
-    
-}
--- .history/static/js/login_20210227160056.js
+++
@@ -1,9 +0,0 @@
-function processloginform() {
-    var username = document.getElementById("username");
-    var password = document.getElementById("password");
-
-    if (username.value == "123" && password.value == "123"){
-        window.location.href="../templates/student_main_page.html";
-        return false;
-    }
-}
