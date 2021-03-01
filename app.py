c5c7fef2c9bdf2e6d866e1585a70a040f6c7161d
YF-0111 <52738321+YF-0111@users.noreply.github.com>
2021-03-01 11:27:01 +0800

    main page

--- app.py
+++ app.py
@@ -6,7 +6,27 @@ app = Flask(__name__)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/student_main_page', methods=['GET', 'POST'])
+def student_main():
+    return render_template('student_main_page.html')
+
+@app.route('/student_within_course/<classcode>', methods=['GET', 'POST'])
+def atudent_within_course(classcode):
+    return render_template('student_within_course.html')
+
+@app.route('/student_get_class/<password>', methods=['GET', 'POST'])
+def student_get_class(password):
+    param = request.get_data()
+    print(param)
+    # get from db
+    classinfo = {
+        'name': 'csc1231',
+        'code': '123456',
+        'info':'other'
+    }
+    return json.dumps(classinfo)
 
 @app.route('/login_test', methods=['GET', 'POST'])
 def login_test():
