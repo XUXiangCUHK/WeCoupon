696a365e371952986523dd4be7b4d5b576bda900
XUXiang <xuxiangcuhk@qq.com>
2021-03-01 23:33:55 +0800

    database update

--- db/manipulator.py
+++ db/manipulator.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import json
-from sql import Sql
+from db.sql import Sql
+
 
 class Manipulator:
     def __init__(self):
         self.sql = Sql()
 
-    def insert_account_info(self, user_name, first_name, last_name, email, SID, password, is_student):
+    def insert_account_info(self, user_name, first_name, last_name, email, SID, password, is_student, activated):
         data = {
             'user_name': user_name,
             'first_name': first_name,
@@ -15,30 +15,53 @@ class Manipulator:
             'email': email,
             'SID': SID,
             'password': password,
-            'is_student': is_student
+            'is_student': is_student,
+            'activated': activated
         }
         self.sql.insert_account(data)
     
-    def insert_course_info(self, course_id, course_name, course_instructor, course_token):
+    def insert_course_info(self, course_code, course_name, course_instructor, course_token):
         data = {
-            'course_id': course_id,
+            'course_code': course_code,
             'course_name': course_name,
             'course_instructor': course_instructor,
             'course_token': course_token,
         }
         self.sql.insert_course(data)
     
-    def insert_enrollment_info(self, user_name, course_id):
+    def insert_enrollment_info(self, user_id, course_id):
         data = {
-            'user_name': user_name,
+            'user_id': user_id,
             'course_id': course_id,
         }
         self.sql.insert_enrollment(data)
 
     def user_enrollment(self, user_name):
         res = self.sql.read_user_course(user_name)
-        return res        
-        
+        return res
+
+    def user_verification(self, email, password):
+        pw = self.sql.read_account_info_by_email(email, ['password'])
+        if pw:
+            if pw[0][0] == password:
+                return True
+        return False
+
+    def user_is_student(self, email):
+        pos = self.sql.read_account_info_by_email(email, ['is_student'])
+        return pos[0][0]
+
+    def fetch_course_info(self, token):
+        res = self.sql.read_course_info(token)
+        if not res:
+            return dict()
+        course_info = {
+            'code': res[0][1],
+            'title': res[0][2],
+            'info': res[0][3]
+        }
+        return course_info
+
 
 if __name__ == "__main__":
     m = Manipulator()
@@ -50,4 +73,8 @@ if __name__ == "__main__":
     # m.insert_enrollment_info('StevenXU', 'IERG3310')
     # m.insert_enrollment_info('StevenXU', 'FTEC3001')
 
-    m.user_enrollment('StevenXU')
+    # m.user_enrollment('StevenXU')
+    # res = m.user_verification('1155107785@link.cuhk.edu.hk', 'wecoupon')
+    # print('res,', res)
+    res = m.user_is_student('1155107785@link.cuhk.edu.hk')
+    print(res)
--- db/sql.py
+++ db/sql.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import json
-from database import Database
+from db.database import Database
+
 
 class Sql:
     def __init__(self):
@@ -10,29 +10,38 @@ class Sql:
     def insert_account(self, data):
         statement = '''
                     INSERT IGNORE INTO `WeCoupon`.`account`
-                    (`user_name`, `first_name`, `last_name`, `email`, `SID`, `password`, `is_student`)
+                    (`user_name`, `first_name`, `last_name`, `email`, `SID`, `password`, `is_student`, `activated`)
                     VALUES
-                    (%(user_name)s, %(first_name)s, %(last_name)s, %(email)s, %(SID)s, %(password)s, %(is_student)s);
+                    (%(user_name)s, %(first_name)s, %(last_name)s, %(email)s, %(SID)s, %(password)s, %(is_student)s, %(activated)s);
                     '''
         self.db.single_write_into_mysql(data, statement)
 
     def insert_course(self, data):
         statement = '''
                     INSERT IGNORE INTO `WeCoupon`.`course`
-                    (`course_id`, `course_name`, `course_instructor`, `course_token`)
+                    (`course_code`, `course_name`, `course_instructor`, `course_token`)
                     VALUES
-                    (%(course_id)s, %(course_name)s, %(course_instructor)s, %(course_token)s);
+                    (%(course_code)s, %(course_name)s, %(course_instructor)s, %(course_token)s);
                     '''
         self.db.single_write_into_mysql(data, statement)
     
     def insert_enrollment(self, data):
         statement = '''
                     INSERT IGNORE INTO `WeCoupon`.`enrollment`
-                    (`user_name`, `course_id`)
+                    (`user_id`, `course_id`)
                     VALUES
-                    (%(user_name)s, %(course_id)s);
+                    (%(user_id)s, %(course_id)s);
                     '''
         self.db.single_write_into_mysql(data, statement)
+
+    def read_account_info_by_email(self, email, column_list):
+        col = ', '.join(column_list)
+        statement = '''SELECT {} FROM WeCoupon.account WHERE email='{}';'''.format(col, email)
+        return self.db.read_from_mysql(statement)
+
+    def read_course_info(self, token):
+        statement = '''SELECT * FROM WeCoupon.course WHERE course_token='{}';'''.format(token)
+        return self.db.read_from_mysql(statement)
 
     def read_user_course(self, user_name):
         statement = '''
@@ -43,12 +52,6 @@ class Sql:
                     '''.format(user_name)
         return self.db.read_from_mysql(statement)
 
+
 if __name__ == "__main__":
-    e = Sql()
-    write_info = {
-            'user_name': 'zhou',
-            'passward': 'zhou',
-            'email': '1155107785@link.cuhk.edu.hk',
-            'pos': 'S'
-        }
-    e.insert_account(write_info)
+    pass
