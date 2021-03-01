840db3ca5d6d2c53e93f06f1a177396ce42dd3e2
YF-0111 <52738321+YF-0111@users.noreply.github.com>
2021-03-01 13:05:03 +0800

    teacher_main_page

---
+++ static/js/teacher_main_page.js
@@ -0,0 +1,14 @@
+var teacher_add_calss = function () {
+    document.getElementById('all_light').style.display = 'block';
+    document.getElementById('contes').style.display = 'block';
+
+    $.getJSON(`http://127.0.0.1:5000/teacher_create_class/${course_name}${course_token}`,(data)=>{
+        let c = data
+        c = { 'code': 'csci3100', 'title': 'Tom', 'info': 'other' }
+        console.log(c)
+        let addClass = `<tr><th><a href="http://127.0.0.1:5000/teacher_within_course/${c.code}">${c.code}</th> <td>${c.title}</td><td>${c.info}</td></tr>`
+        let tbody = document.getElementsByTagName('tbody')[0]
+        tbody.innerHTML = tbody.innerHTML + addClass
+    })
+}
+
