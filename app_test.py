65cba3c8c45b190b07fa829e290530b4dc91be89
SonnetZ <66743225+SonnetZ@users.noreply.github.com>
2021-03-06 17:56:50 +0800

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, abort, flash, url_for, redirect, session
 from flask_bootstrap import Bootstrap
 from forms import LoginForm, RegistrationForm, CreateClassForm
+from flask_mail import Mail
 # from email import send_email
 import json
 
@@ -8,6 +9,13 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'hard to guess string'
+
+# app = Mail(app)
+# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = ''
+# app.config['MAIL_PASSWORD'] = ''
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -20,6 +28,8 @@ def login():
             user_id = 1
             is_student = 1
             session['user_id'] = user_id
+            if input_pw == '0':
+                is_student = 0
 
             next = request.args.get('next')
             if next is None or not next.startswith('/'):
@@ -40,7 +50,13 @@ def sign_up():
         activated = 0
         if form.isInstructor.data:
             is_student = 0
+        # insert into db
         # email verification
+        # token =
+        # user_email = form.email.data
+        # send_email(user_email, 'Confirm Your Account',
+        #       'email/confirm', user=user, token=token)
+
         return redirect(url_for('login'))
     return render_template('signup.html', form=form)
 
@@ -48,7 +64,7 @@ def sign_up():
 def create_course(instructor):
     form = CreateClassForm()
     if form.validate_on_submit():
-        input_token=form.course_token.data
+        input_token = form.course_token.data
         print(input_token)
         # return redirect(url_for('teacher_main_page'))
     return render_template('teacher_create_course.html', form=form)
@@ -56,13 +72,19 @@ def create_course(instructor):
 @app.route('/teacher_main_page', methods=['GET', 'POST'])
 def teacher_main():
     user_id = session['user_id']
-    print(user_id)
-    return render_template('teacher_main_page.html')
+    print('teacher_main')
+    teach_info = [{'code': 'CSCI3100', 'title': 'Software Engineering', 'info': 'Monday'},
+                   {'code': 'IERG3310', 'title': 'Computer Networking', 'info': 'Tuesday'},
+                   {'code': 'FTEC3001', 'title': 'Financial Innovation & Structured Products', 'info': 'Thursday'}]
+    return render_template('teacher_main_page.html', teach_info=teach_info)
 
 
 @app.route('/student_main_page', methods=['GET', 'POST'])
 def student_main():
     user_id = session['user_id']
+    user_msg = request.args['messages']
+    print('student_main')
+    print(user_msg)
     enroll_info = [{'code': 'CSCI3100', 'title': 'Software Engineering', 'info': 'Prof. Michael R. Lyu'},
                    {'code': 'IERG3310', 'title': 'Computer Networking', 'info': 'Prof. Xing Guoliang'},
                    {'code': 'FTEC3001', 'title': 'Financial Innovation & Structured Products', 'info': 'Prof. Chen Nan'}]
@@ -70,13 +92,22 @@ def student_main():
 
 
 @app.route('/student_within_course/<classcode>', methods=['GET', 'POST'])
-def atudent_within_course(classcode):
-    return render_template('student_within_course.html')
+def student_within_course(classcode):
+    print("student_within_course")
+    return render_template('student_within_course.html',course_code=classcode)
 
 
 @app.route('/student_get_class/<password>', methods=['GET', 'POST'])
 def student_get_class(password):
     user_id = session['user_id']
+    print('student_get_class')
+    class_info = {'course_id': 1, 'code': 'CSCI3100', 'title': 'Software Engineering', 'info': 'Prof. Michael R. Lyu'}
+    course_id = class_info['course_id']
+    return json.dumps(class_info)
+
+@app.route('/teacher_create_class/<course_name>', methods=['GET', 'POST'])
+def teacher_create_class(course_name):
+    print('teacher_create_class')
     class_info = {'course_id': 1, 'code': 'CSCI3100', 'title': 'Software Engineering', 'info': 'Prof. Michael R. Lyu'}
     course_id = class_info['course_id']
     return json.dumps(class_info)
@@ -98,11 +129,12 @@ def reward_coupon(username):
 
 @app.route('/teacher_within_course/<classcode>', methods=['GET', 'POST'])
 def teacher_view(classcode):
+    print(classcode)
     question_list = [{'question_id': 'question#1', 'question_type': 'MC'},
                     {'question_id': 'question#2', 'question_type': 'Type'},]
     participation_list = [{'student_id': '1155095222', 'student_name': 'Bob', 'attempt': '20', 'coupon': '1'},
                     {'student_id': '1155095222', 'student_name': 'Peter', 'attempt': '10', 'coupon': '5'}]
-    return render_template('teacher_within_course.html', question_list=question_list, participation_list=participation_list)
+    return render_template('teacher_within_course.html', course_code=classcode, question_list=question_list, participation_list=participation_list)
 
 #@app.route('/teacher_within_course/<classcode>', methods=['GET', 'POST'])
 #def teacher_view_participation(classcode):
