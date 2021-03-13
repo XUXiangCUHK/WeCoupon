2a35653d6c5da1c6a8e3abb86b4cfb3578723df2
timetotiao <1155095200@link.cuhk.edu.hk>
2021-03-13 17:48:25 +0800

    update

--- static/js/teacher_main_page.js
+++ static/js/teacher_main_page.js
@@ -17,3 +17,19 @@ function teacher_add_class() {
     })
 
 }
+
+function display_edit_profile() {
+    document.getElementById('edit_profile').style.display = 'block';
+}
+
+function edit_profile() {
+    document.getElementById('edit_profile').style.display = 'none';
+    var course_token = document.getElementById('token').value;
+
+    $.getJSON(`http://127.0.0.1:5000/student_get_class/${course_token}`,(data)=>{
+        let c = data
+        let addClass = `<tr><th><a href="http://127.0.0.1:5000/student_within_course/${c.course_id}">${c.course_code}</th> <td>${c.course_name}</td><td>${c.course_instructor}</td></tr>`
+        let tbody = document.getElementsByTagName('tbody')[0]
+        tbody.innerHTML = tbody.innerHTML + addClass
+    })
+}
