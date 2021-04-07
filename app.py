79a1a6fdcf1f664f8d67b2285fdc9928628f5e3a
XUXiang <xuxiangcuhk@qq.com>
2021-04-07 22:08:36 +0800

    coupon button

--- app.py
+++ app.py
@@ -238,6 +238,7 @@ def teacher_view_answer(question_id):
     # question_info = mani.fetch_question_info(question_id)
     answer_list = mani.fetch_answer_list(question_id)
     per_ans = mani.fetch_per_ans(current_user.current_course_id, question_id)
+    print(answer_list)
     return render_template('teacher_view_answer.html',
                            question_info=current_user.current_q,
                            answer_list=answer_list,
@@ -270,9 +271,9 @@ def teacher_collect_answer(question_id):
                            per_ans=per_ans)
 
 
-@app.route('/add_coupon/<userid>&<q_id>', methods=['GET', 'POST'])
+@app.route('/add_coupon/<userid>&<q_id>&<a_id>', methods=['GET', 'POST'])
 @login_required
-def reward_coupon(userid, q_id):
+def reward_coupon(userid, q_id, a_id):
     print("Inside add_coupon")
     course_id = mani.fetch_question_info_by_id(q_id, ['course_id'])
     current_user.current_course_id = course_id
@@ -280,6 +281,8 @@ def reward_coupon(userid, q_id):
     current_user.current_q_id = q_id
     current_user.fill_question_info()
     mani.insert_coupon_info(userid, course_id, 0, "reward")
+    print(a_id)
+    sql.update_answer_status(a_id, 1)
     return str()
 
 
@@ -287,8 +290,6 @@ def reward_coupon(userid, q_id):
 @login_required
 def use_coupon(student_id, course_id):
     print("Inside use_coupon")
-    print(student_id)
-    print(course_id)
     current_user.current_course_id = course_id
     current_user.fill_course_info()
     mani.mark_coupon_as_used(student_id, course_id)
