991e8ef3e81e1c3a51833860c7a6824106187b4c
piperzeng <hongni7@126.com>
2021-04-07 11:35:34 +0800

    update teacher collect&view answer js

---
+++ static/js/teacher_collect_answer.js
@@ -0,0 +1,41 @@
+function reward(obj, userid, username) {
+    // var $td = $(obj).parents('tr').children('td');
+    // let username = $td.eq(1).text();
+    // let userid = $td.eq(0).text();
+    var flag = confirm(`Are you sure to reward coupon to ${username}?`);
+    if(flag==true){
+        $.getJSON(`http://127.0.0.1:5000/add_coupon/${userid}`)
+        .always(function(){
+            console.log("success");
+            $(obj).attr('disabled', 'true');
+            $(obj).attr('value', 'Rewarded');
+        })
+        
+        return true;
+    }else {
+        return false;
+    }
+}
+
+function update_answer() {
+    $.ajax({
+        url:"/update_answer",
+        type:"GET",
+        dataType:"JSON",
+        success:function(answer_list){
+            $('#answerList').empty();
+            $.each(answer_list, function (k, v) {
+                var row = "";
+                row += "<tr><td style='width:15%'>"+v.answer_user+
+                "</td>"+"<td style='width:60%'>"+v.answer_content+ 
+                "</td>"+"<td style='width:25%'>"+
+                `<input type='button' class='btn btn-primary' value='Reward' onclick="reward(this, '${v.answer_userid}' , '${v.answer_user}' )"></input></td>`+
+                +"</tr>";
+                $('#answerList').append(row);
+                // answer_userid, answer_user, answer_content
+            })
+        }
+    })
+}
+
+setInterval(update_answer,3000); 
