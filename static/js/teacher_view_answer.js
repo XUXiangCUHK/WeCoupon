c21876710f2aafc1760e2301a5821d57c8eee0c8
timetotiao <1155095200@link.cuhk.edu.hk>
2021-04-10 15:24:55 +0800

    update

--- static/js/teacher_view_answer.js
+++ static/js/teacher_view_answer.js
@@ -7,6 +7,9 @@ function reward(obj, userid, username, q_id, a_id) {
         $.getJSON(`http://127.0.0.1:5000/add_coupon/${userid}&${q_id}&${a_id}`)
         .always(function(){
             console.log("success");
+            document.getElementById(a_id).setAttribute("value", "Rewarded");
+            document.getElementById(a_id).setAttribute("onclick", "");
+            document.getElementById(a_id).setAttribute("class", "btn btn-warning");
             $(obj).attr('disabled', 'true');
             $(obj).attr('value', 'Rewarded');
         })
